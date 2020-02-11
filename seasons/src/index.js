b3b0c78c349b2009de2e5766cb7ecef4bb1672b3
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-02-11 11:58:43 -0800

    Update index.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
-const App = () =>{
-
+class App extends React.Component{
+    render(){
     window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-    );
+        position => console.log(position),
+        err => console.log(err)
+        );
 
-    return <div>Latitude: </div>;
-};
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
