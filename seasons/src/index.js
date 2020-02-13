5a592fe3e59aa88a96e0e94d1183239948f1d8b5
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-02-13 14:20:43 -0800

    update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,44 @@ import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-    render(){
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-        );
+    // not required
 
-        return <div>Latitude: </div>;
+    constructor(props){
+      super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+      this.state={ lat: null, errorMessage: '' };
+
+      window.navigator.geolocation.getCurrentPosition(
+        position =>{
+            // setState
+            this.setState({ lat: position.coords.latitude });
+        },
+        err => {
+            this.setState({errorMessage: err.message})
+        }
+        );
+    }
+
+    // componentDidMount(){
+    //     console.log('My component was rendered to the screen');
+    // }
+
+    // componentDidUpdated(){
+    //     console.log('My component was just updated')
+    // }
+                                                                   
+
+    // we have to define render!!
+    render(){
+     if (this.state.errorMessage && !this.state.lat){
+         return <div>Error: {this.state.errorMessage}</div>
+     }
+
+     if (!this.state.errorMessage && this.state.lat){
+         return <div>Latitude: {this.state.lat}</div>
+     }
+      return <div>Loading!</div>;
     }
 }
 
