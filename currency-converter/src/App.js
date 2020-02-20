687ede09855b19b68f0a175dbf27bb9446409619
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-02-20 12:38:06 -0800

    assignment

---
+++ currency-converter/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import converter from './component/converter';
+
+class App extends Component {
+  render() {
+    return <converter />
+  }
+}
+
+export default App;
+
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component{
-    // not required
 
-    constructor(props){
-      super(props);
+   state = { lat:null, errorMessage:''};
 
-    // THIS IS THE ONLY TIME we do direct assignment to this.state
-      this.state={ lat: null, errorMessage: '' };
-
-      window.navigator.geolocation.getCurrentPosition(
-        position =>{
-            // setState
-            this.setState({ lat: position.coords.latitude });
-        },
-        err => {
-            this.setState({errorMessage: err.message})
-        }
-        );
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({errorMessage: err.message})
+            );
     }
 
-    // componentDidMount(){
-    //     console.log('My component was rendered to the screen');
-    // }
-
-    // componentDidUpdated(){
-    //     console.log('My component was just updated')
-    // }
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+   
+        if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+         return <Loader message="Please accept location request." />;
+       }
                                                                    
-
     // we have to define render!!
     render(){
-     if (this.state.errorMessage && !this.state.lat){
-         return <div>Error: {this.state.errorMessage}</div>
-     }
-
-     if (!this.state.errorMessage && this.state.lat){
-         return <div>Latitude: {this.state.lat}</div>
-     }
-      return <div>Loading!</div>;
-    }
+    return(
+     <div className="border red">
+        {this.renderContent()}
+     </div>
+);
+}
 }
 
 ReactDOM.render(
