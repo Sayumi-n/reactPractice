e15d45ef6e5c1aa6de717f4ea61651236ecd9948
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-03-25 14:53:05 -0700

    sign up page

--- mario-plan/src/components/layout/NavBar.jsx
+++ mario-plan/src/components/layout/NavBar.jsx
@@ -5,20 +5,27 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const NavBar = props => {
-  const { auth } = props;
+  const { auth, profile } = props;
   //   console.log(auth);
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container"></div>
-      <Link to="/" className="brand-logo"></Link>
+      <Link to="/" className="brand-logo">
+        Mario Plan
+      </Link>
       {links}
     </nav>
   );
 };
 const mapStateToProps = state => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   };
 };
 export default connect(mapStateToProps)(NavBar);
